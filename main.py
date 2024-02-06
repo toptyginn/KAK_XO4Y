8df79ea7bed4d2c968d7c647b902b18a317ebd4c
toptyginn <anikonov0307@gmail.com>
2024-02-06 16:21:23 +0700

    Show_map

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Это сделал Арсений
 import os
+import sys
+
 import pygame
 import requests
 
@@ -17,20 +19,44 @@ def get_address_coords(address):
     return toponym_coodrinates
 
 
+def show_map(ll_spn=None, map_type="map", add_params=None):
+    if ll_spn:
+        map_request = f'http://static-maps.yandex.ru/1.x/?{ll_spn}&l={map_type}'
+    else:
+        map_request = f'http://static-maps.yandex.ru/1.x/?l={map_type}'
+    if add_params:
+        map_request += '&' + add_params
+    response = requests.get(map_request)
+
+    if not response:
+        print("Ошибка запроса")
+        print(map_request)
+        print('Http статус:', response.status_code, '(', response.reason, ')')
+        sys.exit(1)
+
+    map_file = "map.png"
+    try:
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    except Exception as e:
+        print(f'ERROR: {e}')
+
+
 coords = get_address_coords('Австралия').split()
-map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords[0]},{coords[1]}&spn=40.000,0.002&l=map"
-response = requests.get(map_request)
+ll_spn = f'll={coords[0]},{coords[1]}&spn=1,1'
 
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
 
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+def main():
+    pygame.init()
+    while pygame.event.wait().type != pygame.QUIT:
+        screen = pygame.display.set_mode((600, 450))
+        show_map(ll_spn, 'map')
+        screen.blit(pygame.image.load('map.png'), (0, 0))
+        pygame.display.flip()
+    pygame.quit()
 
-os.remove(map_file)
+    os.remove('map.png')
+
+
+if __name__ == '__main__':
+    main()
