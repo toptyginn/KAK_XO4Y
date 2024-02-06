e11cae482a233013b39c12eda17ca40608184287
kvshavlenko <150430919+kvshavlenko@users.noreply.github.com>
2024-02-06 16:40:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import pygame
 import requests
 
 
-def open_map(spn):
-    coords = get_address_coords('Австралия').split()
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={coords[0]},{coords[1]}&spn={spn},0.002&l=map"
+def open_map(spn, discry, discrx):
+    coords = get_address_coords('Аляска').split()
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={str(float(coords[0]) + discrx)},{str(float(coords[1]) + discry)}&spn={spn},0.002&l=map"
     response = requests.get(map_request)
     map_file = "map.png"
     with open(map_file, "wb") as file:
@@ -38,21 +38,42 @@ with open(map_file, "wb") as file:
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 running = True
+discry = 0
+discrx = 0
 while running:
-    screen.blit(pygame.image.load(map_file), (0, 0))
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            os.remove(map_file)
-            if event.key == pygame.K_PAGEUP:
-                if spn + 20 != 200:
-                    spn += 20
-            if event.key == pygame.K_PAGEDOWN:
-                if spn - 20 != 0:
-                    spn -= 20
-            open_map(spn)
+    try:
+        screen.blit(pygame.image.load(map_file), (0, 0))
+
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                os.remove(map_file)
+                if event.key == pygame.K_PAGEUP:
+                    if spn + 5 != 200:
+                        spn += 5
+                if event.key == pygame.K_PAGEDOWN:
+                    if spn - 5 != 0:
+                        spn -= 5
+
+                if event.key == pygame.K_UP:
+                    discry += 5
+                    print(discry)
+                if event.key == pygame.K_DOWN:
+                    discry -= 5
+                    print(discry)
+                if event.key == pygame.K_RIGHT:
+                    discrx += 5
+                    print(discrx)
+                if event.key == pygame.K_LEFT:
+                    discry -= 5
+                    print(discrx)
+                open_map(spn, discry, discrx)
+    except Exception:
+        print("Ошибка")
+        running = False
+
 pygame.quit()
 
 os.remove(map_file)
